6807e25de2d913c72dfcf08b050a75b4a211273e
Kevin Navarro <ken314588@gmail.com>
2020-11-27 21:12:16 -0600

    no message

--- tiendaonline-master/config/config.php
+++ tiendaonline-master/config/config.php
@@ -1,9 +1,9 @@
 <?php 
 
-$servidor=" ecommerceaplicada.database.windows.net";
+$servidor="ecommerceaplicada.database.windows.net";
 $usuario="admin123";
 $contrasena="KNavarro*1";
-$bd="EcommerceDataBase";
+$bd="phpsitioweb";
 
 $conexion=mysql_connect($servidor,$usuario,$contrasena) or die ('No se puede conectar a la base de Datos');
 $basededatos=mysql_select_db($bd,$conexion) or die ('No existe conexion con la base de datos');
